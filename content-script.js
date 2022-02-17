b60f6ee227af04132ca640f9c9fd7502aff18688
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-17 22:57:03 +0300

    I have added the "Reading QR Code" Logic

--- content-script.js
+++ content-script.js
@@ -2,9 +2,14 @@ const create = document.getElementById("create-btn");
 const menu = document.getElementById("menu");
 const submit = document.getElementById("submit");
 const createMenu = document.getElementById("create-menu");
-const output = document.getElementById("output");
+const outputCreate = document.getElementById("output_create");
+const outputRead = document.getElementById("output_read");
 const input = document.getElementById("input");
 const content = document.getElementById("content");
+const readMenu = document.getElementById("read-menu");
+const read = document.getElementById("read");
+const readBtn = document.getElementById("read-btn");
+const link = document.getElementById("link");
 
 create.addEventListener("click", () => {
   menu.classList.add("hidden");
@@ -13,17 +18,33 @@ create.addEventListener("click", () => {
 if (submit) {
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    output.classList.remove("hidden");
+    outputCreate.classList.remove("hidden");
     if (input.value.trim().length > 0) {
-      output.innerHTML = `<img src="http://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=100x100"></img>
+      outputCreate.innerHTML = `<img src="http://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=100x100"></img>
       <button class="download"><i class="fa fa-download"></i> Download</button>`;
       input.value = "";
     } else {
       const content = `<p id="error">Please enter a valid link.</p>`;
-      output.insertAdjacentHTML("beforeend", content);
+      outputCreate.insertAdjacentHTML("beforeend", content);
       setTimeout(function () {
         document.getElementById("error").remove();
       }, 500);
     }
+  });
+}
+
+if (read) {
+  readBtn.addEventListener("click", function () {
+    menu.classList.add("hidden");
+    readMenu.classList.remove("hidden");
+    read.addEventListener("click", function (e) {
+      e.preventDefault();
+      outputRead.classList.remove("hidden");
+      fetch(`https://api.qrserver.com/v1/read-qr-code/?fileurl=${link.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          outputRead.innerHTML = `<a href="${data[0]["symbol"][0]["data"]}">Link</a>`;
+        });
+    });
   });
 }
