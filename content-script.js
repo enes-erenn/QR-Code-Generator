4186f694fef44c750e5eaabec48854d0a12fa01a
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-18 19:24:02 +0300

    General Improvements

--- content-script.js
+++ content-script.js
@@ -1,61 +1,71 @@
-const createBtn = document.getElementById("create-btn");
-const menu = document.getElementById("menu");
-const create = document.getElementById("submit");
-const createMenu = document.getElementById("create-menu");
-const outputCreate = document.getElementById("output_create");
-const outputRead = document.getElementById("output_read");
-const input = document.getElementById("input");
-const content = document.getElementById("content");
-const readMenu = document.getElementById("read-menu");
-const read = document.getElementById("read");
-const readBtn = document.getElementById("read-btn");
-const link = document.getElementById("link");
-const loader = document.getElementById("loader-el");
-const reload = document.getElementById("main-page");
-
+const createBtn = document.getElementById("create-btn"),
+  menu = document.getElementById("menu"),
+  create = document.getElementById("submit"),
+  createMenu = document.getElementById("create-menu"),
+  outputCreate = document.getElementById("output_create"),
+  outputRead = document.getElementById("output_read"),
+  input = document.getElementById("input"),
+  content = document.getElementById("content"),
+  readMenu = document.getElementById("read-menu"),
+  read = document.getElementById("read"),
+  readBtn = document.getElementById("read-btn"),
+  link = document.getElementById("link"),
+  loader = document.getElementById("loader-el"),
+  reload = document.getElementById("main-page");
+let error = '<p id="error">Please enter a valid link.</p>',
+  invalid = "<p id='invalid'>Invalid request</p>";
 createBtn.addEventListener("click", () => {
-  menu.classList.add("hidden");
-  reload.classList.remove("hidden");
-  createMenu.classList.remove("hidden");
-});
-
-readBtn.addEventListener("click", () => {
-  menu.classList.add("hidden");
-  reload.classList.remove("hidden");
-  readMenu.classList.remove("hidden");
-});
-
-create.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (input.value.trim().length > 0) {
-    outputCreate.innerHTML = `<img src="http://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=100x100"></img>
-      <a href="http://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=100x100" download><button class="download">Download</button></a>`;
-    input.value = "";
-  } else {
-    const content = `<p id="error">Please enter a valid link.</p>`;
-    outputCreate.insertAdjacentHTML("beforeend", content);
-    setTimeout(function () {
-      document.getElementById("error").remove();
-    }, 500);
-  }
-});
-
-read.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (link.value.trim().length > 0) {
-    outputRead.classList.remove("hidden");
-    fetch(`https://api.qrserver.com/v1/read-qr-code/?fileurl=${link.value}`)
-      .then((response) => response.json())
-      .then((data) => {
-        outputRead.innerHTML = `<a href="${data[0]["symbol"][0]["data"]}" target="_blank"><button class="custom-btn btn-style"><span>Link</span></button></a>`;
-      })
-      .catch(() => {
-        outputRead.innerHTML = "Invalid request";
-      });
-  }
-});
-if (reload) {
-  reload.addEventListener("click", () => {
-    location.reload();
-  });
-}
+  menu.classList.add("hidden"),
+    reload.classList.remove("hidden"),
+    createMenu.classList.remove("hidden");
+}),
+  readBtn.addEventListener("click", () => {
+    menu.classList.add("hidden"),
+      reload.classList.remove("hidden"),
+      readMenu.classList.remove("hidden");
+  }),
+  create.addEventListener("click", (e) => {
+    e.preventDefault(),
+      input.value.trim().length > 0
+        ? ((outputCreate.innerHTML = `<img src="http://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=100x100"></img>\n      <a href="http://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=100x100" download><button class="download">Download</button></a>`),
+          (input.value = ""))
+        : (document.getElementById("error") ||
+            document
+              .getElementById("error-container")
+              .insertAdjacentHTML("afterend", error),
+          setTimeout(function () {
+            document.getElementById("error").remove();
+          }, 500));
+  }),
+  read.addEventListener("click", (e) => {
+    e.preventDefault(),
+      link.value.trim().length > 0
+        ? (outputRead.classList.remove("hidden"),
+          fetch(
+            `https://api.qrserver.com/v1/read-qr-code/?fileurl=${link.value}`
+          )
+            .then((e) => e.json())
+            .then((e) => {
+              outputRead.innerHTML = `<a href="${e[0].symbol[0].data}" target="_blank"><button class="custom-btn btn-style"><span>Link</span></button></a>`;
+            })
+            .catch(() => {
+              loader.classList.add("hidden"),
+                document
+                  .getElementById("error-container")
+                  .insertAdjacentHTML("afterend", invalid),
+                setTimeout(function () {
+                  document.getElementById("invalid").remove();
+                }, 500);
+            }))
+        : (document.getElementById("error") ||
+            document
+              .getElementById("error-container")
+              .insertAdjacentHTML("afterend", error),
+          setTimeout(function () {
+            document.getElementById("error").remove();
+          }, 500));
+  }),
+  reload &&
+    reload.addEventListener("click", () => {
+      location.reload();
+    });
